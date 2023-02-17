0e9c7f9bab4707138b80f395d925ebe7659e0485
Azis Rosyid <azisrosyid321@gmail.com>
2023-02-17 23:45:44 +0700

    Update

--- src/dataMhs.java
+++ src/dataMhs.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class dataMhs {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
