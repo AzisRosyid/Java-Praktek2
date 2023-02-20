27d123f40e0c47c82298da0ac854bb69a77432ac
Azis Rosyid <azisrosyid321@gmail.com>
2023-02-20 20:52:51 +0700

    Update

--- src/dataMhs.java
+++ src/dataMhs.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class dataMhs {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static ArrayList<Mahasiswa> data = new ArrayList<>();
+    private static Mahasiswa[] data;
 
     public static void main(String[] args) throws Exception {
         while(true) {
@@ -36,6 +35,7 @@ public class dataMhs {
     private static void inputData() throws Exception {
         System.out.print("\n<=== Input Data ===>\nMasukkan jumlah data Mahasiswa : ");
         int mhs = Integer.parseInt(br.readLine());
+        data = new Mahasiswa[mhs];
         for (int i = 0; i < mhs; i++) {
             System.out.print("\nMasukkan Nama Mahasiswa : ");
             String nama = br.readLine();
@@ -43,7 +43,7 @@ public class dataMhs {
             String alamat = br.readLine();
             System.out.print("Masukkan IPK Mahasiswa : ");
             double ipk = Double.parseDouble(br.readLine());
-            data.add(new Mahasiswa(nama, alamat, ipk));
+            data[i] = new Mahasiswa(nama, alamat, ipk);
             tampilData(data);
         }
     }
@@ -51,11 +51,9 @@ public class dataMhs {
     private static void pencarianData() throws Exception {
         System.out.print("\n<=== Pencarian Data ===>\nMasukkan Nama Mahasiswa : ");
         String cari = br.readLine();
-        for (int i = 0; i < data.size(); i++)
-            if (cari.equals(data.get(i).getNama())) {
-                ArrayList<Mahasiswa> mhs = new ArrayList<>();
-                mhs.add(data.get(i));
-                tampilData(mhs);
+        for (int i = 0; i < data.length; i++)
+            if (cari.equals(data[i].getNama())) {
+                tampilData(new Mahasiswa[] {data[i]});
                 System.out.println("Data Mahasiswa " + cari + " ditemukan!");
                 return;
             }
@@ -65,9 +63,16 @@ public class dataMhs {
     private static void penghapusanData() throws Exception {
         System.out.print("\n<=== Penghapusan Data ===>\nMasukkan Nama Mahasiswa  : ");
         String hapus = br.readLine();
-        for (int i = 0; i < data.size(); i++)
-            if (hapus.equals(data.get(i).getNama())) {
-                data.remove(data.get(i));
+        for (int i = 0; i < data.length; i++)
+            if (hapus.equals(data[i].getNama())) {
+                Mahasiswa[] temp = new Mahasiswa[data.length-1];
+                for (int j = 0; j < data.length; j++)
+                    if (j == i) continue;
+                    else if (j < i) 
+                        temp[j] = data[j];
+                    else 
+                        temp[j-1] = data[j];
+                data = temp;
                 System.out.println("-------------------------\nData Mahasiswa " + hapus + " ditemukan dan dihapus!");
                 return;
             }
@@ -79,9 +84,10 @@ public class dataMhs {
         tampilData(data);
     }
 
-    private static void tampilData(ArrayList<Mahasiswa> mhs) {
+    private static void tampilData(Mahasiswa[] mhs) {
         int nama = 6, alamat = 7, ipk = 5;
         for (Mahasiswa i: mhs) {
+            if (i == null) break;
             if ((i.getNama().length() + 2) > nama) 
                 nama = i.getNama().length() + 2;
             if ((i.getAlamat().length() + 2) > alamat) 
@@ -93,8 +99,10 @@ public class dataMhs {
         System.out.println("\n" + "-".repeat(spc));
         System.out.printf("%" + -nama + "s||  %" + -alamat + "s||  %" + -ipk + "s", "Nama", "Alamat", "IPK");
         System.out.print("\n" + "-".repeat(spc));
-        for (Mahasiswa i: mhs) 
+        for (Mahasiswa i: mhs) {
+            if (i == null) break;
             System.out.printf("\n%" + -nama + "s||  %" + -alamat + "s||  %" + -ipk + "s", i.getNama(), i.getAlamat(), i.getIpk());
+        }
         System.out.println("\n" + "-".repeat(spc));
     }
 }
